5a33d79b6797b95f26f37b3efb2b2f4953117e31
tanmay <tanmayjoshidun@gmail.com>
2022-08-20 16:45:10 +0530

    node mailer minor changes

--- mailers/reset_password.js
+++ mailers/reset_password.js
@@ -1,14 +1,12 @@
 const nodemailer = require('../config/nodemailer');
-const url = require('url');
-// const urlObject = url.parse(req.url, true);
+require('dotenv').config();
 module.exports.resetPassword = async function(user, link){
     try{   
-        // let link = `${req.protocol}://${req.host}/reset-password/${token}`;
         
         const htmlString = nodemailer.renderTemplate({user: user, link: link}, '/reset_password.ejs');
         
         let info = await nodemailer.transporter.sendMail({
-            from: '"Expense Manager" <expensemanager1206@outlook.com>', // sender address
+            from: `"Expense Manager" <${process.env.user}>`, // sender address
             to: user.email, // list of receivers
             subject: "Reset Password", // Subject line
             html: htmlString, // html body
--- mailers/verify_account.js
+++ mailers/verify_account.js
@@ -1,14 +1,12 @@
 const nodemailer = require('../config/nodemailer');
-
+require('dotenv').config();
 module.exports.verifyAccount = async function(user, link){
     try{
-
-        // let link = `http://localhost:3000${relLink}`;
         
         let htmlString = nodemailer.renderTemplate({user: user, link: link}, '/verification_mail.ejs');
         
         let info = await nodemailer.transporter.sendMail({
-            from: '"Expense Manager" <expensemanager1206@outlook.com>', // sender address
+            from: `"Expense Manager" <${process.env.user}>`, // sender address
             to: user.email, // list of receivers
             subject: "Account verification", // Subject line
             html: htmlString, // html body
